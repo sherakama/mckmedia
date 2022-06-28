5b8b5f6c59028a0b1f0547c4a8f04f5997080f70
Shea McKinney <sherakama@gmail.com>
2022-06-28 09:02:23 -0700

    Progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
     extend: {},
   },
   plugins: [
+     // Add our own variants for convenience
+     plugin(({ addVariant }) => {
+      addVariant('hocus', ['&:hover', '&:focus']);
+      addVariant('group-hocus', [
+        ':merge(.group):focus &',
+        ':merge(.group):hover &',
+      ]);
+      addVariant('children', '& > *');
+    }),
+
+    // 3rd Party Plugins;
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
 }
