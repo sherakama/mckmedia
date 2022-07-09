25ccd35d35d86ec5afbddb3c4765f92e0da8a408
Shea McKinney <sherakama@gmail.com>
2022-07-09 12:02:28 -0700

    Google font fixes.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=optional" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lobster&display=optional" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lobster+Two&display=optional" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet" type="text/css" />
       </Head>
       <body>
         <Main />
